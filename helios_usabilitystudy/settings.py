0eedd59ff7cf980cd8d5a3cf4614ec51ef23add9
Karola Marky <yonjuni@pinyto.de>
2016-12-09 17:13:52 +0200

    Collection of static files

--- helios_usabilitystudy/settings.py
+++ helios_usabilitystudy/settings.py
@@ -118,3 +118,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "helios_main/static"),
+    os.path.join(BASE_DIR, "helios_institutes/static"),
+    os.path.join(BASE_DIR, "helios_smartphone/static"),
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
