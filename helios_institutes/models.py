21fae9a80d7d8297a9cb20e95545f525ffa78239
Karola Marky <yonjuni@pinyto.de>
2016-12-09 13:37:50 +0200

    DB fields added

--- helios_institutes/models.py
+++ helios_institutes/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Subjects(models.Model):
+    subject_id = models.CharField('Subject ID', max_length=30, unique=True, editable=False)
+    subject_token = models.CharField('Subject_Token', max_length=30, unique=False, editable=False)
+
+
+class TimeSpent (models.Model):
+    overall_duration = models.BigIntegerField('Overall (Millisec)', editable=False)
+    voting_duration = models.BigIntegerField('Voting (Millisec)', editable=False)
+    verification_duration = models.BigIntegerField('Verification (Millisec)', editable=False)
+
+
+class Choices(models.Model):
+    question = models.CharField(max_length=2000)
+    option1 = models.CharField(max_length=2000)
+    option2 = models.CharField(max_length=2000)
+    option3 = models.CharField(max_length=2000)
+
