079ab2fdbbc78706450b94cb199cac04ae8ddf50
Karola Marky <yonjuni@pinyto.de>
2016-12-09 14:17:40 +0200

    Reorganization of files, adding of css

---
+++ helios_main/urls.py
@@ -0,0 +1,8 @@
+# from django.conf.urls import patterns, url
+#
+# urlpatterns = patterns(
+#     '',
+#     url(r'^helios$', 'helios_main.views.TODO', name='TODO'),
+#     url(r'^booth$', 'helios_main.views.TODO', name='TODO'),
+#     url(r'^submit', 'helios_main.views.TODO', name='TODO'),
+# )
---
+++ static/js/app.js
@@ -0,0 +1,42 @@
+'use strict';
+
+var heliosStudyApp = angular.module(
+    "heliosStudy",
+    [
+        "ngAnimate",
+        "ngRoute",
+        "localization",
+        "BackendService"
+    ]
+).config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+    $routeProvider.
+        // when('/:id', {templateUrl: '/selectionTask/static/partials/introduction.html', controller: 'introductionCtrl'}).
+        // when('/task/:id', {templateUrl: '/selectionTask/static/partials/task.html', controller: 'taskCtrl'}).
+        // when('/thanks/:id', {templateUrl: '/selectionTask/static/partials/thanks.html', controller: 'thanksCtrl'}).
+        // when('/error/:id/:error', {templateUrl: '/selectionTask/static/partials/error.html', controller: 'errorCtrl'}).
+        // otherwise({templateUrl: '/selectionTask/static/partials/introduction.html', controller: 'introductionCtrl'});
+    // use the HTML5 History API
+    $locationProvider.html5Mode(true).hashPrefix('!');
+}]);
+
+heliosStudyApp.run(function ($rootScope, $window, $document, localize) {
+    // root scope functions
+    $rootScope.getLanguages = function () {
+        return ['en', 'jp'];
+    };
+    $rootScope.$watch('language', function (newLang) {
+        localize.setLanguage(newLang);
+        $rootScope.$broadcast('langChange', newLang);
+    });
+    // initialization
+    if (!$rootScope.language) {
+        $rootScope.language = $window.navigator.userLanguage ||
+                              $window.navigator.language ||
+                              $document.getElementsByTagName('html')[0].lang;
+        if ($rootScope.language && ($rootScope.language.length > 2)) {
+            $rootScope.language = $rootScope.language.substr(0, 2);
+        }
+        //If hard coding a language is desired, do it here, if not delete the following line
+        $rootScope.language = "jp";
+    }
+});
