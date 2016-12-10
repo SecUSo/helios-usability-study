edd9b4dd541c4a0031af188e4d2b15f1d1a6d801
Karola Marky <yonjuni@pinyto.de>
2016-12-10 16:35:12 +0200

    Static files main submodule moved

--- helios_main/views.py
+++ helios_main/views.py
@@ -4,5 +4,5 @@ import os
 
 
 def home(request):
-    with open(os.path.join(os.path.dirname(__file__), "../helios_main/static/index.html"), 'r') as index_html_file:
+    with open(os.path.join(os.path.dirname(__file__), "../helios_main/static/helios_main/index.html"), 'r') as index_html_file:
         return HttpResponse(index_html_file.read(), content_type='text/html')
