0dee0361392b8d43076d6f58d31343bac875e7c9
Karola Marky <yonjuni@pinyto.de>
2016-12-10 17:38:57 +0200

    Apps added to settings

--- helios_usabilitystudy/settings.py
+++ helios_usabilitystudy/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'helios_usabilitystudy',
+    'helios_main',
+    'helios_institutes',
+    'helios_smartphone',
 ]
 
 MIDDLEWARE = [
