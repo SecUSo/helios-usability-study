f60acd7a66c0f32370a945937e360e3b2379ab75
Karola Marky <yonjuni@pinyto.de>
2016-12-10 20:02:29 +0200

    static files integrated in URLs

--- helios_main/urls.py
+++ helios_main/urls.py
@@ -1,9 +1,9 @@
-from django.conf import settings
 from django.conf.urls import url
 from helios_main import views
-from django.conf.urls.static import static
 
-urlpatterns = static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + [
+
+urlpatterns =\
+[
     url(r'^.*', views.home, name='home'),
     # url(r'^main$', views.introduction, name='introduction'),
     # url(r'^booth$', 'helios_main.views.TODO', name='TODO'),
--- helios_usabilitystudy/urls.py
+++ helios_usabilitystudy/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from helios_usabilitystudy import views
+from django.conf.urls.static import static
+from django.conf import settings
 admin.autodiscover()
 
-urlpatterns = [
+urlpatterns = urlpatterns = static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + [
     url(r'^admin/', admin.site.urls),
     url(r'^login$', views.login, name='login'),
     url(r'^main/', include('helios_main.urls')),
