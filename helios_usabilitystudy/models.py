557646359df69665d68de34b73297e7debf14196
Karola Marky <yonjuni@pinyto.de>
2016-12-11 14:47:18 +0200

    Questions and Options splitted, opention code for "decrpytion" integrated

--- helios_usabilitystudy/models.py
+++ helios_usabilitystudy/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 
-class Subjects(models.Model):
+class Subject(models.Model):
     subject_id = models.CharField('Subject ID', max_length=30, unique=True, primary_key=True, editable=False)
     subject_token = models.CharField('Subject_Token', max_length=30, unique=False, editable=False)
     experiment_type = models.CharField('Experiment_Type', max_length=30, unique=False, editable=False)
@@ -12,14 +12,14 @@ class Subjects(models.Model):
     verification_duration = models.BigIntegerField('Verification (Millisec)', editable=False)
 
 
-class Options(models.Model):
+class Option(models.Model):
     option_code = models.CharField('Option_code', max_length=4, unique=True, primary_key=True, editable=False)
     option = models.CharField('Option', max_length=2000, editable=False)
 
 
-class Questions(models.Model):
+class Question(models.Model):
     question = models.CharField('Question', max_length=2000, editable=False)
-    option = models.ManyToManyField(Options)
+    option = models.ManyToManyField(Option)
 
 
 
