3f761ce572126ecf835d3c12d318fd33c88c399d
Karola Marky <yonjuni@pinyto.de>
2016-12-11 17:59:18 +0200

    Username in URL

--- helios_usabilitystudy/views.py
+++ helios_usabilitystudy/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
 from django.contrib.auth import login as django_login
 from django import forms
+from django.views.decorators.csrf import csrf_exempt
 import os
 
 
@@ -11,6 +12,7 @@ def welcome(request):
     return render(request, 'helios_usabilitystudy/welcome.html', {})
 
 
+@csrf_exempt
 def login(request):
     print(request.POST)
     username = request.POST['id']
@@ -23,7 +25,7 @@ def login(request):
     user = authenticate(username=username, password=password)
     if user is not None:
         django_login(request, user)
-        return redirect('main/')
+        return redirect('main/' + username)
     else:
         print("User doesn't exist")
         return redirect('login/')
