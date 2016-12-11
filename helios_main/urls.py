8597ff784ecc2210794daef1808ba03dd04f89ed
Karola Marky <yonjuni@pinyto.de>
2016-12-11 22:45:19 +0200

    Assin in URLs

--- helios_main/urls.py
+++ helios_main/urls.py
@@ -4,7 +4,5 @@ from helios_main import views
 
 urlpatterns = [
     url(r'^.*', views.home, name='home'),
-    # url(r'^main$', views.introduction, name='introduction'),
-    # url(r'^booth$', 'helios_main.views.TODO', name='TODO'),
-    # url(r'^submit', 'helios_main.views.TODO', name='TODO'),
+    url(r'^assign$', views.assign, name='assign'),
 ]
