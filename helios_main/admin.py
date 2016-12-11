5b92f3502fb4cd489a96a4a93252cb3f684b5aa0
Karola Marky <yonjuni@pinyto.de>
2016-12-11 15:10:11 +0200

    Durations added to admin page

--- helios_main/admin.py
+++ helios_main/admin.py
@@ -2,7 +2,13 @@ from django.contrib import admin
 from helios_usabilitystudy.models import Subject
 from helios_usabilitystudy.models import Option
 from helios_usabilitystudy.models import Question
+from helios_usabilitystudy.models import Duration
 
-admin.site.register(Subject)
+
 admin.site.register(Option)
 admin.site.register(Question)
+admin.site.register(Duration)
+admin.site.register(Subject)
+
+
+
