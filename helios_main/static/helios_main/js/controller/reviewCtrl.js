e497185b581d5cd589b941552901eab62d8947ee
Karola Marky <yonjuni@pinyto.de>
2016-12-20 09:30:23 +0200

    Review page (structure) added js and html

---
+++ helios_main/static/helios_main/js/controller/reviewCtrl.js
@@ -0,0 +1,21 @@
+'use strict';
+
+heliosStudyMainApp.controller("reviewCtrl", function ($scope, $routeParams, $location, Backend) {
+    $scope.auditButton = function () {
+
+        //    $location.path('review/' + $routeParams['id']);
+
+    }
+
+    $scope.castVoteButton = function () {
+
+        //    $location.path('review/' + $routeParams['id']);
+
+    }
+
+     $scope.backButton = function () {
+
+        //    $location.path('review/' + $routeParams['id']);
+
+    }
+});
