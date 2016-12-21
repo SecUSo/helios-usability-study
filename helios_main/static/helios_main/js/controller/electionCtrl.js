a9915856034bd60051ead31785976cd320741caf
Karola Marky <yonjuni@pinyto.de>
2016-12-21 12:51:33 +0200

    Question displaced :)

--- helios_main/static/helios_main/js/controller/electionCtrl.js
+++ helios_main/static/helios_main/js/controller/electionCtrl.js
@@ -1,10 +1,19 @@
 'use strict';
 
 heliosStudyMainApp.controller("electionCtrl", function ($scope, $routeParams, $location, Backend) {
+
+    Backend.assign($routeParams['id']).success(function (data) {
+        var result = angular.fromJson(data);
+        if ("Error" in result) {
+            $location.path('/error/' + $routeParams['id'] + '/' + result["Error"]);
+        }
+        $scope.experimentData = result;
+        $scope.experimentData.question = result.question.question;
+        $scope.startTimeAll = Date.now();
+    });
+
     $scope.proceedButton = function () {
         //$scope.startTime = Date.now();
         $location.path('review/' + $routeParams['id']);
-
-
-     }
+    }
 });
