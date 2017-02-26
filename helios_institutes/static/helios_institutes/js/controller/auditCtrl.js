39f7aad7427dc15a855b27673e3f3c88f8359f85
Karola Marky <yonjuni@pinyto.de>
2017-02-26 23:36:11 +0100

    http request added

--- helios_institutes/static/helios_institutes/js/controller/auditCtrl.js
+++ helios_institutes/static/helios_institutes/js/controller/auditCtrl.js
@@ -1,9 +1,11 @@
 'use strict';
 
-heliosStudyInstitutesApp.controller("auditCtrl", function ($rootScope, $scope, $routeParams, $window, $location) {
+heliosStudyInstitutesApp.controller("auditCtrl", function ($rootScope, $scope, $routeParams, $window, $location, $http) {
 
     $scope.show_progress = false;
     $scope.vote = $routeParams['encrypted_vote'];
+    $scope.ballot_tracker = btoa($scope.vote).toString().substr(0, 16);
+    console.log($scope.ballot_tracker);
 
 
     //From institutes back to review
@@ -12,8 +14,10 @@ heliosStudyInstitutesApp.controller("auditCtrl", function ($rootScope, $scope, $
     };
 
     $scope.redirectToInstituteButton = function () {
+        $http.post('//127.0.0.1:8080/verifier_one', "vote:" + $scope.vote + "tracker:" + $scope.ballot_tracker);
         $window.open('//127.0.0.1:8080/verifier_one', '_blank');
         $location.path('election/' + $routeParams['id']);
+
     };
 
 });
