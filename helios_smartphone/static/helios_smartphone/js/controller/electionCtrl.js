ac9da46f41ce083e527d9b7b6b57a84f037aa87f
Karola Marky <yonjuni@pinyto.de>
2017-03-01 15:21:14 +0100

    Handling of invalid votes

--- helios_smartphone/static/helios_smartphone/js/controller/electionCtrl.js
+++ helios_smartphone/static/helios_smartphone/js/controller/electionCtrl.js
@@ -62,19 +62,6 @@ heliosStudySmartphoneApp.controller("electionCtrl", function ($scope, $routePara
         }
     };
 
-
-    $scope.saveChoiceInvalid = function (choice) {
-
-        if ($rootScope.selected_code == "00") {
-            $rootScope.selected_code = null;
-        } else {
-            $rootScope.selected_code = "00";
-            $rootScope.choice = choice;
-            console.log('Choice is ' + $rootScope.choice);
-        }
-    };
-
-
     //Pseudo-encryption of the vote. Well, the option code is hidden between random values.
     function encrypt() {
         for (var i = 0; i < 100; i++) {
