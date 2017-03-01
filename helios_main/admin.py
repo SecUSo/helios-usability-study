9abf73823c131bc504475def3fdd91673419ab2a
Karola Marky <yonjuni@pinyto.de>
2017-03-01 16:03:05 +0100

    Added Excel export for duration data

--- helios_main/admin.py
+++ helios_main/admin.py
@@ -1,14 +1,52 @@
 from django.contrib import admin
+from django.http import HttpResponse
+from django.utils.encoding import smart_str
 from helios_usabilitystudy.models import Subject
 from helios_usabilitystudy.models import Option
 from helios_usabilitystudy.models import Question
 from helios_usabilitystudy.models import Duration
+import csv
 
 
+def export_csv(duration, request, queryset):
+
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename=results.csv'
+    writer = csv.writer(response, csv.excel)
+    response.write(u'\ufeff'.encode('utf8'))
+    writer.writerow([
+        smart_str(u"Subject ID"),
+        smart_str(u"Has verified?"),
+        smart_str(u"Instruction"),
+        smart_str(u"Verification number"),
+        smart_str(u"Overall"),
+        smart_str(u"Voting"),
+        smart_str(u"Verification"),
+
+    ])
+    for obj in queryset:
+        writer.writerow([
+            smart_str(obj.subject),
+            smart_str(obj.has_verified),
+            smart_str(obj.instruction_duration),
+            smart_str(obj.verification_number),
+            smart_str(obj.overall_duration),
+            smart_str(obj.voting_duration),
+            smart_str(obj.verification_duration),
+        ])
+    return response
+export_csv.short_description = u"Export CSV"
+
+
+class MyDurationAdmin(admin.ModelAdmin):
+    actions = [export_csv]
+    list_display = (
+    'subject', 'has_verified', 'instruction_duration', 'verification_number', 'overall_duration', 'voting_duration', 'verification_duration')
+
+
+admin.site.register(Duration, MyDurationAdmin)
 admin.site.register(Option)
 admin.site.register(Question)
-admin.site.register(Duration)
 admin.site.register(Subject)
-
 
 
