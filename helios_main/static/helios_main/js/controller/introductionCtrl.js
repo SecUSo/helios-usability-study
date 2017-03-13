7dcd5810fa2278185e27fdc3fba464a8062bbed5
Karola Marky <yonjuni@pinyto.de>
2017-03-13 22:01:04 +0100

    Saving added for intro

--- helios_main/static/helios_main/js/controller/introductionCtrl.js
+++ helios_main/static/helios_main/js/controller/introductionCtrl.js
@@ -2,7 +2,10 @@
 
 heliosStudyMainApp.controller("introductionCtrl", function (Backend, $rootScope, $scope, $routeParams, $location) {
 
+    $rootScope.subject = $location.path().split("/")[2];
+
     $scope.startButton = function () {
+        Backend.save_timestamp($rootScope.subject, new Date().getTime(), "Overall start");
         $location.path('election/' + $routeParams['id']);
     }
 });
